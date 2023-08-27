5cc3523a3544232a1617f2e89b17f352e00ebb97
Vishal Rastogi <maganuk@hotmail.com>
2023-08-27 15:42:32 +0530

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginResponse, OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monocloud-angular-sample';
+  isAuthenticated = false;
+  userData$: any;
+
+  constructor(public oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe((loginResponse: LoginResponse) => {
+      const { isAuthenticated, userData } = loginResponse;
+      this.isAuthenticated = isAuthenticated;
+      this.userData$ = userData;
+    });
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff().subscribe();
+  }
 }
